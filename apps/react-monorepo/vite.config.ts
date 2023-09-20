6ef66e1698473684318802d258ef0a783f7a81dc
jglee96 <zakklee96@gmail.com>
2023-09-20 19:18:25 +0900

    add: install, config vite-plugin-compression2

--- apps/react-monorepo/vite.config.ts
+++ apps/react-monorepo/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/monorepo-compression',
@@ -16,7 +17,7 @@ export default defineConfig({
     host: 'localhost',
   },
 
-  plugins: [react(), nxViteTsPaths()],
+  plugins: [react(), nxViteTsPaths(), compression()],
 
   // Uncomment this if you are using workers.
   // worker: {
